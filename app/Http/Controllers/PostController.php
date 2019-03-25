d165d85902a0d537e8793dcf618b709ccfb1a2f6
Jorge Fernández <jorge.fernandez@inoma.mx>
2019-03-25 17:29:26 -0600

    Inicio CRUD Post
    Empezando configuración para CRUD Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Post;
 
 class PostController extends Controller {
-    //
+    
+    /** 
+     * Constructor para verificar Token en controladores
+     */
+    public function __construct() {
+        $this->middleware('api.auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
+     * Traer todos los post
+     */
+    public function index() {
+        $posts = Post::all();
+
+        return response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'posts' => $posts
+        ]);
+    }
+
 }
